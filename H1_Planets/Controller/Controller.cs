f8f35d330f75bdd8edd9bc9a1d960b8b37996398
Nicklas Lindegaard Martlev Gustavsen <nicklasgustavsen75@gmail.com>
2023-09-13 13:34:25 +0200

    Update

--- H1_Planets/Controller/Controller.cs
+++ H1_Planets/Controller/Controller.cs
@@ -19,15 +19,15 @@ namespace H1_Planets.Controller
         public void Start()
         {
             #region Planets
-            Planet mercury = new Planet("Merkur", 167, 4879.4f);
-            Planet venus = new Planet("Venus", 462, 12104f);
-            Planet earth = new Planet("Jorden", 15, 12742f);
-            Planet mars = new Planet("Mars", -60, 6779f);
-            Planet jupiter = new Planet("Jupiter", -145, 142800f);
-            Planet saturn = new Planet("Saturn", -178, 116460f);
-            Planet uranus = new Planet("Uranus", -195, 50724f);
-            Planet neptune = new Planet("Neptun", -214, 55528f);
-            Planet pluto = new Planet("Pluto", -232, 2376.6f);
+            Planet mercury = new Planet("Merkur", 167, 4879, 0.330f, 5427, 3.7f, 1407.6f, 4222.6f, 57.9f, 88f, 47.4f, 0, false);
+            Planet venus = new Planet("Venus", 464, 12104, 4.87f, 5243, 8.9f, -5832.5f, 2802f, 108.2f, 224.7f, 35f, 0, false);
+            Planet earth = new Planet("Jorden", 15, 12756, 5.97f, 5514, 9.8f, 23.9f, 24.0f, 149.6f, 365.2f, 29.8f, 1, false);
+            Planet mars = new Planet("Mars", -65, 6792, 0.642f, 3933, 3.7f, 24.6f, 24.7f, 227.9f, 687f, 24.1f, 2, false);
+            Planet jupiter = new Planet("Jupiter", -110, 142984, 1898f, 1326, 23.1f, 9.9f, 9.9f, 778.6f, 4331f, 13.1f, 67, true);
+            Planet saturn = new Planet("Saturn", -140, 120536, 568f, 687, 9f, 10.7f, 10.7f, 1433.5f, 10747f, 9.7f, 62, true);
+            Planet uranus = new Planet("Uranus", -195, 51118, 86.8f, 1271, 8.7f, -17.2f, 17.2f, 2872.5f, 30589f, 6.8f, 27, true);
+            Planet neptune = new Planet("Neptun", -200, 49528, 102f, 1638, 11f, 16.1f, 16.1f, 4495.1f, 59.8f, 5.4f, 14, true);
+            Planet pluto = new Planet("Pluto", -225, 2370, 0.0146f, 2095, 0.7f, -153.3f, 153.3f, 5906.4f, 90.56f, 4.7f, 5, true);
             #endregion
              
             List<Planet> planets = new List<Planet>()
--- H1_Planets/Model/Planet.cs
+++ H1_Planets/Model/Planet.cs
@@ -13,7 +13,17 @@ namespace H1_Planets.Model
     {
         private string _name;
         private float _temperature;
-        private float _diameter;
+        private int _diameter;
+        private float _mass;
+        private short _density;
+        private float _gravity;
+        private float _rotationPeriod;
+        private float _dayLength;
+        private float _sunDistance;
+        private float _orbitPeriod;
+        private float _orbitVelocity;
+        private ushort _moonAmount;
+        private bool _hasRingSystem;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="Planet"/> class, using multiple arguments
@@ -21,11 +31,21 @@ namespace H1_Planets.Model
         /// <param name="name">name of planet</param>
         /// <param name="temperature">temperatur of planet</param>
         /// <param name="diameter">diameter of planet</param>
-        public Planet(string name, float temperature, float diameter)
+        public Planet(string name, float temperature, int diameter, float mass, short density, float gravity, float rotationPeriod, float dayLength, float sunDistance, float orbitPeriod, float orbitVelocity, ushort moonAmount, bool hasRingSystem)
         {
             _name = name;
             _temperature = temperature;
             _diameter = diameter;
+            _mass = mass;
+            _density = density;
+            _gravity = gravity;
+            _rotationPeriod = rotationPeriod;
+            _dayLength = dayLength;
+            _sunDistance = sunDistance;
+            _orbitPeriod = orbitPeriod;
+            _orbitVelocity = orbitVelocity;
+            _moonAmount = moonAmount;
+            _hasRingSystem = hasRingSystem;
         }
 
         public string Name
@@ -40,10 +60,70 @@ namespace H1_Planets.Model
             set { _temperature = value; }
         }
 
-        public float Diameter
+        public int Diameter
         {
             get { return _diameter; }
             set { _diameter = value; }
+        }
+
+        public float Mass
+        {
+            get { return _mass; }
+            set { _mass = value; }
+        }
+
+        public short Density
+        {
+            get { return _density; }
+            set { _density = value; }
+        }
+
+        public float Gravity
+        {
+            get { return _gravity; }
+            set { _gravity = value; }
+        }
+
+        public float RotationPeriod
+        {
+            get { return _rotationPeriod; }
+            set { _rotationPeriod = value; }
+        }
+
+        public float DayLength
+        {
+            get { return _dayLength; }
+            set { _dayLength = value; }
+        }
+
+        public float SunDistance
+        {
+            get { return _sunDistance; }
+            set { _sunDistance = value; }
+        }
+
+        public float OrbitPeriod
+        {
+            get { return _orbitPeriod; }
+            set { _orbitPeriod = value; }
+        }
+
+        public float OrbitVelocity
+        {
+            get { return _orbitVelocity; }
+            set { _orbitVelocity = value; }
+        }
+
+        public ushort MoonAmount
+        {
+            get { return _moonAmount; }
+            set { _moonAmount = value; }
+        }
+
+        public bool HasRingSystem
+        {
+            get { return _hasRingSystem; }
+            set { _hasRingSystem = value; }
         }
     }
 }
